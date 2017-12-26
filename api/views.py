98c0c40b58afa42cc95de49915cb526a5f4af7c7
owen135731 <owensawyer95@gmail.com>
2017-12-26 17:17:12 -0400

    endpoint fixes..

--- api/views.py
+++ api/views.py
@@ -18,28 +18,30 @@ def index(request):
 
 @api_view(['POST'])
 def get_summoner(request):
-    response = aggregate.get_summoner(request.POST.get('region'), request.POST.get('name'))
+    print(request.POST)
+    print(request.data)
+    response = aggregate.get_summoner(request.data['region'], request.data['name'])
     return response_handler(response) #userSerializer?
 
 @api_view(['POST'])
 def get_matchlist(request):
-    response = aggregate.get_matchlist(request.POST.get('region'), request.POST.get('accountId'))
+    response = aggregate.get_matchlist(request.data['region'], request.data['accountId'])
     return response_handler(response) #matchlistSerializer?
 
 @api_view(['POST'])
 def get_match(request):
-    response = aggregate.get_match(request.POST.get('region'), request.POST.get('matchId'))
-    match = serialize_match(request.POST.get('accountId'), response)
+    response = aggregate.get_match(request.data['region'], request.data['matchId'])
+    match = serialize_match(request.data['accountId'], response)
     return response_handler(match)
 
 @api_view(['POST'])
 def get_popular_runes_for_champion(request):
-    response = rune.get_popular_runes_for_champ(request.POST.get('champion'), request.POST.get('role'))
+    response = rune.get_popular_runes_for_champ(request.data['champion'], request.data['role'])
     return response_handler(response)
 
 @api_view(['POST'])
 def get_rune_info(request):
-    rune_info = rune.get_rune_info(request.POST.get('rune'))
+    rune_info = rune.get_rune_info(request.data['rune'])
     return response_handler(rune_info)
 
 def response_handler(response):
